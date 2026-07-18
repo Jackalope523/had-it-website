0821cde1c365893410329579cccdfbd68c0f74b9
EthanCote5233 <ECote523@Gmail.com>
2026-07-18 19:27:07 -0400

    Uncomment it

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -91,7 +91,7 @@ export default async function RootLayout({
           <Header />
           {children}
           <Footer />
-          {/* <Chat /> */}
+          <Chat />
           {!accepted ? <CookieBanner /> : null}
         </NextIntlClientProvider>
       </body>
--- components/Chat.tsx
+++ components/Chat.tsx
@@ -3,14 +3,14 @@
 import { EventHandlerPayload, LiveChatWidget } from '@livechat/widget-react';
 
 export default function CookieBanner() {
-  
+
   function handleNewEvent(event: EventHandlerPayload<'onNewEvent'>) {
     console.log('LiveChatWidget.onNewEvent', event)
   }
 
   return   <LiveChatWidget
       license="19845957"
-      visibility="maximized"
+     
       onNewEvent={handleNewEvent}
     />;
 }
