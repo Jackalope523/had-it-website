e1c70b68619069b5e38c9f731726446d20e0fbd0
EthanCote5233 <ECote523@Gmail.com>
2026-07-20 14:40:23 -0400

    Prettier

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,24 +1,26 @@
-'use client';
+"use client";
 
-import { EventHandlerPayload, LiveChatWidget } from '@livechat/widget-react';
-import { useSearchParams } from 'next/navigation';
-import { CONCERN_CODES } from '@/lib/concerns';
+import { EventHandlerPayload, LiveChatWidget } from "@livechat/widget-react";
+import { useSearchParams } from "next/navigation";
+import { CONCERN_CODES } from "@/lib/concerns";
 
 export default function Chat() {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
-  const code = searchParams.get('c');
+  const code = searchParams.get("c");
   const concern = code ? CONCERN_CODES[code] : undefined;
-  const customerName = concern ? `Had It (${concern})` : 'Had It';
+  const customerName = concern ? `Had It (${concern})` : "Had It";
 
-  function handleNewEvent(event: EventHandlerPayload<'onNewEvent'>) {
-    console.log('LiveChatWidget.onNewEvent', event)
+  function handleNewEvent(event: EventHandlerPayload<"onNewEvent">) {
+    console.log("LiveChatWidget.onNewEvent", event);
   }
 
-  return   <LiveChatWidget
+  return (
+    <LiveChatWidget
       license="19845957"
       customerName={customerName}
-      visibility='minimized'
+      visibility="minimized"
       onNewEvent={handleNewEvent}
-    />;
+    />
+  );
 }
