e4ba7ade2cb066cd4e3b6bbecac9e6700948ced9
EthanCote5233 <ECote523@Gmail.com>
2026-08-31 13:02:58 -0400

    Add language tag to customer name

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -5,11 +5,26 @@ import { useSearchParams } from "next/navigation";
 import { CONCERN_CODES } from "@/lib/concerns";
 
 export default function Chat() {
+  function getLanguageLabel() {
+    if (typeof window === "undefined") return "EN";
+
+    const hostname = window.location.hostname.replace(/^www\./, "");
+
+    if (hostname === "ras-le-bol.ca") {
+      return "FR";
+    }
+
+    return "EN";
+  }
+
   const searchParams = useSearchParams();
 
   const code = searchParams.get("c");
   const concern = code ? CONCERN_CODES[code] : undefined;
-  const customerName = concern ? `Had It (${concern})` : "Had It";
+  const language = getLanguageLabel();
+  const customerName = concern
+    ? `Had It (${concern}) [${language}]`
+    : `Had It [${language}]`;
 
   function handleNewEvent(event: EventHandlerPayload<"onNewEvent">) {
     console.log("LiveChatWidget.onNewEvent", event);
